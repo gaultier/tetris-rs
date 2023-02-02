9e6b5fc88e0ce8288e4b7626cfb0250763529b05
Philippe Gaultier <philippe.gaultier@ppro.com>
2023-02-02 15:48:20 +0100

    Revert "[wip]"
    This reverts commit 2b6eeea15952cfd7ba0445cab85c5688a9d4c1e9.

--- src/vert.glsl
+++ src/vert.glsl
@@ -10,16 +10,14 @@ layout(location = 2) in vec3 color;
 layout(location = 0) out vec3 out_color;
 
 layout(set = 0, binding = 0) uniform Data {
-    /* mat4 world; */
-    /* mat4 view; */
-    /* mat4 proj; */
-    vec2 tetramino_offset;
+    mat4 world;
+    mat4 view;
+    mat4 proj;
 } uniforms;
 
 void main() {
-    /* mat4 worldview = uniforms.view * uniforms.world; */
-    /* gl_Position = uniforms.proj * worldview * vec4(position + position_offset, 0.0, 1.0); */
-    gl_Position = vec4(position + position_offset + uniforms.tetramino_offset, 0.0, 1.0);
+    mat4 worldview = uniforms.view * uniforms.world;
+    gl_Position = uniforms.proj * worldview * vec4(position + position_offset, 0.0, 1.0);
 
     out_color = color;
 }
